835aeb298c01f7143b1621e7d0fed3bf19ae176b
Nick Schuch <nick@previousnext.com.au>
2021-05-18 13:57:30 +1000

    Extra handling for grep in script: mysql/scripts/mysql-drop-tables.sh

--- mysql/scripts/mysql-drop-tables.sh
+++ mysql/scripts/mysql-drop-tables.sh
@@ -16,7 +16,7 @@ if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
     DATABASE_PASSWORD="$2"
     DATABASE_HOSTNAME="$3"
     DATABASE_NAME="$4"
- 
+
     if [ -z $DATABASE_HOSTNAME ]; then
         fatal "Not found: hostname"
     fi
@@ -36,11 +36,15 @@ if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
     # Connection string which will be used to perform operations on the MySQL database.
     CONNECTION_STRING="mysql --host=$DATABASE_HOSTNAME --user=$DATABASE_USERNAME --password=$DATABASE_PASSWORD $DATABASE_NAME"
 
-    TABLES=$($CONNECTION_STRING -e 'show tables' | awk '{ print $1}' | grep -v '^Tables' )
+    # Grep is wrapped in { COMMAND || true; } because it returns an exit code of 1 if no results are found.
+    # We have set -e for this script so if we don't do this wrapping the script will exit right here if no tables are found.
+    # This script will fail on new environments with an empty database and break initial imports.
+    TABLES=$($CONNECTION_STRING -e 'show tables' | awk '{ print $1}' | { grep -v '^Tables' || true; } )
 
     if [ "$TABLES" == "" ]
     then
-        fatal "Error - No tables found in $DATABASE_NAME database!"
+        info "No tables found in $DATABASE_NAME database!"
+        exit 0
     fi
 
     for table in $TABLES
